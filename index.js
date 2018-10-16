8ec423c76824133b84c970d0cad3ac6986ce10cd
Simone <simonebellano@hotmail.com>
2018-10-16 09:22:18 +0200

    added verify payment method

--- index.js
+++ index.js
@@ -11,34 +11,99 @@ const XPAY_CONF = {
   XPAY_SERVLET_URL: process.env.XPAY_SERVLET_URL || 'https://coll-ecommerce.keyclient.it/ecomm/ecomm/DispatcherServlet'
 };
 
-module.exports = function(configuration) {
+const MODULE_NAME = 'xpay-nodejs';
 
-  // init configuration
-  configuration = configuration || {}; // init default, need to work on 5.7
-  const xpayConfiguration = underscore.extend(XPAY_CONF, configuration);
+/**
+ * XPay Nexi Service
+ */
+class XPay {
+    /**
+     * class constructor
+     */
+    constructor(){
 
-  // get the url
-  return function getUrl(transactionCode, quantity, macCode) {
+    }
 
-    // calculate the mac
-    let shasum = crypto.createHash('sha1');
-    let hashpayload = util.format('codTrans=%sdivisa=EURimporto=%s%s', transactionCode, quantity, xpayConfiguration.XPAY_KEY);
-    shasum.update(hashpayload);
-    macCode = shasum.digest('hex');
+    /**
+     * init
+     * @param configuration
+     */
+    configure(configuration){
+        // init configuration
+        configuration = configuration || {}; // init default, need to work on 5.7
+        this._xpayConfiguration = underscore.extend(XPAY_CONF, configuration);
+    }
+    /**
+     * get payment nexi xpay url
+     * @param transactionCode, quantity, macCode
+     */
+    getUrl(options, macCode) {
 
-    // create the link
-    let xpayLink = util.format(
-      '%s?mac=%s&url_back=%s&alias=%s&url=%s&importo=%s&divisa=EUR&codTrans=%s',
-      xpayConfiguration.XPAY_SERVLET_URL,
-      macCode,
-      xpayConfiguration.XPAY_RETURN_URL_KO,
-      xpayConfiguration.XPAY_ALIAS,
-      xpayConfiguration.XPAY_RETURN_URL_OK,
-      quantity,
-      transactionCode
-    );
+        // calculate the mac
+        let shasum = crypto.createHash('sha1');
+        let hashpayload = util.format('codTrans=%sdivisa=%simporto=%s%s', options.codTrans, options.divisa, options.importo, this._xpayConfiguration.XPAY_KEY);
+        shasum.update(hashpayload);
+        macCode = shasum.digest('hex');
 
-    // done
-    return xpayLink;
-  };
+        // create the link
+        let xpayLink = util.format(
+            '%s?mac=%s&url_back=%s&alias=%s&url=%s&importo=%s&divisa=EUR&codTrans=%s',
+            this._xpayConfiguration.XPAY_SERVLET_URL,
+            macCode,
+            this._xpayConfiguration.XPAY_RETURN_URL_KO,
+            this._xpayConfiguration.XPAY_ALIAS,
+            this._xpayConfiguration.XPAY_RETURN_URL_OK,
+            options.importo,
+            options.codTrans
+        );
+
+        // done
+        return xpayLink;
+    };
+
+    /**
+     * verify payment transaction
+     * @param nexi response, macCode
+     */
+    verify(xpayResponse) {
+
+        //check nexi response required params
+        var requiredParams = ['codTrans', 'esito', 'importo', 'divisa', 'data', 'orario', 'codAut', 'mac'];
+        var emptyParams = [];
+        requiredParams.forEach(function(param, index){
+            if (param in xpayResponse) {
+                //continue
+            }else{
+                emptyParams.push(param);
+            }
+        });
+
+        if(emptyParams.length>0) return false; //if not found params
+
+        // calculate the mac
+        let shasum = crypto.createHash('sha1');
+        let hashpayload = util.format('codTrans=%sesito=%simporto=%sdivisa=%sdata=%sorario=%scodAut=%s%s',
+            xpayResponse.codTrans, xpayResponse.esito, xpayResponse.importo, xpayResponse.divisa, xpayResponse.data, xpayResponse.orario, xpayResponse.codAut,
+            this._xpayConfiguration.XPAY_KEY);
+        shasum.update(hashpayload);
+        var macCalculated = shasum.digest('hex');
+
+        // Verifico corrispondenza tra MAC calcolato e MAC di ritorno
+        if (macCalculated != xpayResponse.mac) {
+            return false;
+        }
+
+        if (xpayResponse.esito == 'OK') {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    };
+
+
+
 }
+
+module.exports = XPay;
--- test/xpay-spec.js
+++ test/xpay-spec.js
@@ -2,29 +2,65 @@
 
 'use strict';
 
-describe('xpay integration', function() {
 
-  var xpay = require('../index')({
-    XPAY_KEY: '123456789012345678901',
-    XPAY_ALIAS: '7654321',
-    XPAY_RETURN_URL_OK: 'http://www.google.com',
-    XPAY_RETURN_URL_KO: 'http://www.bing.com',
-  });
+describe('xpay get payment url', function() {
+
+    var XPay = require('../index.js');
+    var xpay = new XPay();
+
+    xpay.configure({
+        XPAY_KEY: 'XPAY_KEY',
+        XPAY_ALIAS: 'ALIAS_WEB_TEST',
+        XPAY_RETURN_URL_OK: 'http://www.google.com',
+        XPAY_RETURN_URL_KO: 'http://www.bing.com',
+        XPAY_SERVLET_URL: 'https://int-ecommerce.nexi.it/ecomm/ecomm/DispatcherServlet',
+    });
 
 
-  it('should get a url', function(done) {
-    var args = {
-      transactionCode: 'ABC123000001',
-      quantity: 1000 // 10 euro
-    };
+    it('should get a url', function(done) {
+        var args = {
+            codeTrans: 'ABC123000001',
+            importo: 1000 // 10 euro
+        };
 
-    var mac;
+        var mac;
 
-var res = xpay(args.transactionCode, args.quantity, mac);
-    expect(res).toBeDefined();
-    // TODO: think about more testing
-    console.log(res);
-    done();
-  });
-
+        var res = xpay.getUrl(args, mac);
+        expect(res).toBeDefined();
+        // TODO: think about more testing
+        console.log(res);
+        done();
+    });
 });
+
+
+describe('xpay verify payment', function() {
+
+    var XPay = require('../index.js');
+    var xpay = new XPay();
+
+    xpay.configure({
+        XPAY_KEY: 'XPAY_KEY',
+        XPAY_ALIAS: 'ALIAS_WEB_TEST',
+
+    });
+
+    it('should get a bool', function(done) {
+        var xpayResponse = {
+            codTrans:'ABC123000001',
+            esito:'OK',
+            importo:1000,
+            divisa:'EUR',
+            data:'20181015',
+            orario:'235711',
+            codAut:'CODAUTH',
+            mac:'MAC',
+        };
+
+        var res = xpay.verify(xpayResponse);
+        expect(res).toBe(true);
+        // TODO: think about more testing
+        done();
+    });
+});
+
